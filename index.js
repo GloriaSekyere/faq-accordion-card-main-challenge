6699fea5418cc56feb862efbdf6b13939322f3f3
GloriaSekyere <gloriasekyere15@gmail.com>
2022-01-16 08:27:19 +0000

    fix background issues and add box shadow to main

---
+++ index.js
@@ -0,0 +1,101 @@
+function arrow1() {
+
+    let question = document.getElementById("q1").style;
+
+    let answer = document.getElementById("a1").style;
+
+    let arrow = document.getElementById('arrow-1').style;
+
+    if (answer.display == 'block') {
+        question.fontWeight = 400;
+        answer.display = 'none';
+        arrow.transform  = 'rotate(0deg)';
+          }
+    else {
+        question.fontWeight = 700;
+        answer.display = 'block';
+        arrow.transform  = 'rotate(180deg)';
+          }   
+    }
+      
+function arrow2() {
+
+  let question = document.getElementById("q2").style;
+
+  let answer = document.getElementById("a2").style;
+
+  let arrow = document.getElementById('arrow-2').style;
+
+  if (answer.display == 'block') {
+      question.fontWeight = 400;
+      answer.display = 'none';
+      arrow.transform  = 'rotate(0deg)';
+  }
+  else {
+      question.fontWeight = 700;
+      answer.display = 'block';
+      arrow.transform  = 'rotate(180deg)';
+  }   
+}
+
+function arrow3() {
+
+  let question = document.getElementById("q3").style;
+
+  let answer = document.getElementById("a3").style;
+
+  let arrow = document.getElementById('arrow-3').style;
+
+  if (answer.display == 'block') {
+      question.fontWeight = 400;
+      answer.display = 'none';
+      arrow.transform  = 'rotate(0deg)';
+  }
+  else {
+      question.fontWeight = 700;
+      answer.display = 'block';
+      arrow.transform  = 'rotate(180deg)';
+  }   
+}
+
+function arrow4() {
+
+  let question = document.getElementById("q4").style;
+
+  let answer = document.getElementById("a4").style;
+
+  let arrow = document.getElementById('arrow-4').style;
+
+  if (answer.display == 'block') {
+      question.fontWeight = 400;
+      answer.display = 'none';
+      arrow.transform  = 'rotate(0deg)';
+  }
+  else {
+      question.fontWeight = 700;
+      answer.display = 'block';
+      arrow.transform  = 'rotate(180deg)';
+  }   
+}
+
+function arrow5() {
+
+  let question = document.getElementById("q5").style;
+
+  let answer = document.getElementById("a5").style;
+
+  let arrow = document.getElementById('arrow-5').style;
+
+  if (answer.display == 'block') {
+      question.fontWeight = 400;
+      answer.display = 'none';
+      arrow.transform  = 'rotate(0deg)';
+  }
+  else {
+      question.fontWeight = 700;
+      answer.display = 'block';
+      arrow.transform  = 'rotate(180deg)';
+  }   
+}
+          
+      
